a011fb8017e05aa7adb5d250fc1fa5582ea1b8aa
Martín <martin17@gmail.com>
2012-08-24 16:24:13 -0300

    Update Ext.ux.api.Fullscreen.js

--- Ext.ux.api.Fullscreen.js
+++ Ext.ux.api.Fullscreen.js
@@ -3,7 +3,7 @@
  * Work with all modern browsers.
  * Components that have floating children do not work properly. 
  * This happens because these children are not children of the component in the DOM tree.
- * 
+ * Based on http://johndyer.name/native-fullscreen-javascript-api-plus-jquery-plugin/
  * @author Martín Panizzo <martin@fotolounge.com.ar>
  * @version 0.1-alpha
  */
